abd3fd1c77659d00dbaabdf3bd386a7d6111eac1
Trevor Brown <admin@stratus3d.com>
2017-10-20 17:47:05 +0300

    More work on the translit tests. Fix several issues.

--- spec/translit_spec.rb
+++ spec/translit_spec.rb
@@ -4,23 +4,26 @@ describe "translit" do
   let(:russian) { "Транслитерация между кириллицей и латиницей с коммандной строки или в твоей программе" }
   let(:russian_transliteration) { "Transliteratsiya mezhdu kirillitsej i latinitsej s kommandnoj stroki ili v tvoej programme" }
   let(:english) { "Transliteration between cyrillic <-> latin from command-line or your program" }
+  let(:ukrainian) { "Транслітерація між кирилицею <-> Латинська з командного рядка або вашої програми" }
+  let(:ukrainian_transliteration) { "Transliteratsiya mizh kyrylytseyu <-> Latyns'ka z komandnoho ryadka abo vashoyi prohramy" }
   # This is broken
   let(:english_transliteration) { "" }
+  let(:ukrainian_english_transliteration) { "Транслітератіон бетвеен cирілліc <-> латін фром cомманд-ліне ор иоур проґрам" }
 
-  it "transliterates from english to russian" do
+  it "transliterates from russian to english" do
     expect(Translit.convert(russian, :english)).to eq(russian_transliteration)
   end
 
-  it "transliterates from english to ukrainian" do
-    pending
+  it "transliterates from ukrainian to english" do
+    expect(Translit.convert(ukrainian, :english)).to eq(ukrainian_transliteration)
   end
 
-  it "transliterates from russian to english " do
+  it "transliterates from english to russian" do
     # FIXME: This is broken, not sure why
-    # expect(Translit.convert(english, :russian)).to eq(english_transliteration)
+    #expect(Translit.convert(english, :russian)).to eq(english_transliteration)
   end
 
-  it "transliterates from ukrainian to english " do
-    pending
+  it "transliterates from english to ukrainian" do
+    expect(Translit.convert(english, :ukrainian)).to eq(ukrainian_english_transliteration)
   end
 end
