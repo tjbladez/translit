82f33b244144e2c5e838fdfec3d841ebf1c26023
Trevor Brown <admin@stratus3d.com>
2017-10-20 16:50:41 +0300

    Add rspec.

---
+++ spec/translit_spec.rb
@@ -0,0 +1,17 @@
+describe "translit" do
+  it "transliterates from english to russian" do
+    pending
+  end
+
+  it "transliterates from english to ukrainian" do
+    pending
+  end
+
+  it "transliterates from russian to english " do
+    pending
+  end
+
+  it "transliterates from ukrainian to english " do
+    pending
+  end
+end
